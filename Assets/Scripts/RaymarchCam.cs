28385be8346b4a4b4c16b40202981567423280bb
Chu Hao Wen <chuhaow@gmail.com>
2023-02-24 19:29:47 -0600

    Added Shape rendering

--- Assets/Scripts/RaymarchCam.cs
+++ Assets/Scripts/RaymarchCam.cs
@@ -33,69 +33,88 @@ public class RaymarchCam : MonoBehaviour
 
     private void Start()
     {
-        renderTexture = new RenderTexture(256, 256, 24);
-        renderTexture.enableRandomWrite = true;
-        renderTexture.Create();
-        raymarch.SetTexture(0, "Result", renderTexture);
-        raymarch.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
+        //renderTexture = new RenderTexture(256, 256, 24);
+       // renderTexture.enableRandomWrite = true;
+       // renderTexture.Create();
+       // raymarch.SetTexture(0, "Result", renderTexture);
+       // raymarch.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         cam = Camera.current;
-        shapes = new List<RaymarchShape>(FindObjectsOfType<RaymarchShape>());
+        //shapes = new List<RaymarchShape>(FindObjectsOfType<RaymarchShape>());
         shapeDataArr = GetShapes();
-        if (shapeDataArr.Length > 0)
-        {
-            SetShaderParam();
-        }
+        
         if (renderTexture == null || renderTexture.height != Screen.height || renderTexture.width != Screen.width)
         {
-            if(renderTexture != null)
+            if (renderTexture != null)
             {
                 renderTexture.Release();
-                
+
             }
             renderTexture = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
             renderTexture.enableRandomWrite = true;
             renderTexture.Create();
 
         }
-
-        raymarch.SetTexture(0, "Result", renderTexture);
+        if (shapeDataArr.Length > 0)
+        {
+            SetShaderParam();
+        }
+        //Debug.Log(destination);
+        raymarch.SetTexture(0, "source", source);
+        raymarch.SetTexture(0, "dest", renderTexture);
+        //raymarch.SetTexture(0, "Result", renderTexture);
         raymarch.SetFloat("Resolution", renderTexture.width);
         raymarch.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
 
         Graphics.Blit(renderTexture, destination);
 
-        shapeBuffer.Dispose();
+        if(shapeBuffer != null)
+        {
+            shapeBuffer.Dispose();
+        }
+        
     }
 
     private ShapeData[] GetShapes()
     {
         List<RaymarchShape> list = new List<RaymarchShape>(FindObjectsOfType<RaymarchShape>());
-        ShapeData[] result = new ShapeData[shapes.Count];
+        ShapeData[] result = new ShapeData[list.Count];
+
         for (int i = 0; i < list.Count; i++)
         {
+            
             result[i].type = list[i].GetShape();
             result[i].position = list[i].GetPosition();
             result[i].scale = list[i].GetScale();
             result[i].color = list[i].GetColor();
+            
         }
         return result;
     }
 
     private void SetShaderParam()
     {
+        
         //Shape data
         int shapeDataBytes = sizeof(int) + sizeof(float)*(3+3+4);
         shapeBuffer = new ComputeBuffer(shapeDataArr.Length, shapeDataBytes);
-        raymarch.SetBuffer(0, "shapeBuffer", shapeBuffer);
+        shapeBuffer.SetData(shapeDataArr);
+        ShapeData[] temp = new ShapeData[2];
+        //shapeBuffer.GetData(temp);
+        //Debug.Log(temp[0].position);
+        //Debug.Log(shapeBuffer[0].position);
+        raymarch.SetBuffer(0, "shapes", shapeBuffer);
+        raymarch.SetInt("shapesLen", shapeDataArr.Length);
         //Render Texture
         //Cam to World matrix
         raymarch.SetMatrix("CamToWorldMatrix", cam.cameraToWorldMatrix);
         //Cam inverse matrix 
         raymarch.SetMatrix("InverseProjMatrix", cam.projectionMatrix.inverse);
         //Lights
+
+        
     }
 }
